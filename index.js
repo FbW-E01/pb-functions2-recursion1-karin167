02d1968c24ce9aff9e317e2886e4eac5cc1f2e64
karin yahud <karin167bz@gmail.com>
2021-05-10 13:30:56 +0200

    karin

---
+++ index.js
@@ -0,0 +1,18 @@
+    const array = [1, 2, 3, 4, 5, 6];
+      let sumTotal = 0
+    const sum = (arr)=> {
+  arr.forEach(x => {
+      sumTotal += x;
+  });
+  return sumTotal;
+    }
+console.log(sum(array));
+
+
+
+//   for(let i = 0; i<arr.length; i++){
+// return sumTotal += arr[i];
+//     }
+
+
+
