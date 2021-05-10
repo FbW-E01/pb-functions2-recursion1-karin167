3aed788bcfbc8e048c72ab0de549d78cdb28c1db
karin yahud <karin167bz@gmail.com>
2021-05-10 13:49:22 +0200

    karin 2

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-    const array = [1, 2, 3, 4, 5, 6];
+// This is Normal Summing
+
+   const array = [1, 2, 3, 4, 5, 6];
       let sumTotal = 0
     const sum = (arr)=> {
-  arr.forEach(x => {
-      sumTotal += x;
-  });
-  return sumTotal;
+      arr.forEach(x => {
+              sumTotal += x;
+          });
+          return sumTotal;
+            }
+        console.log(sum(array));
+        
+        
+// This is with N numbers  
+        
+const array = [1, 2, 3, 4, 5, 6];
+
+let totalNumber =0
+let sumNumbers = 0
+
+const sumUpToN = (item)=>{
+    if(item.length === 1){
+        console.log(item[0]);
+    }else{
+       let nItem = item.pop() // we create N numbers
+       array.forEach(element => {
+           sumNumbers+=element // here we sum all the elements together instead of last Item
+       });
+       console.log(sumNumbers + nItem);
     }
-console.log(sum(array));
+}
 
-
-
-//   for(let i = 0; i<arr.length; i++){
-// return sumTotal += arr[i];
-//     }
-
-
-
+sumUpToN(array)
